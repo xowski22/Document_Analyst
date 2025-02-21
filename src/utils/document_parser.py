1bede251fb61b2e6f676d19b73a6a5dc8cb4ba0e
xowski22 <xowski22@gmail.com>
2025-02-21 18:11:23 +0100

    finished basic methods in parser

--- src/utils/document_parser.py
+++ src/utils/document_parser.py
@@ -3,6 +3,7 @@ import PyPDF2
 import docx
 import re
 
+
 class DocumentParser:
     def __init__(self):
         self.supported_formats = ['.pdf', '.docx', '.txt']
@@ -21,17 +22,33 @@ class DocumentParser:
 
     def _parse_pdf(self, file_path:str) -> str:
         """Parses a PDF file"""
-        pass
+        with open(file_path, 'rb') as pdf_file:
+            pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+            text = ''
+
+            for page in pdf_reader.pages:
+                text += page.extractText()
+
+            return text
 
     def _parse_docx(self, file_path:str) -> str:
         """Parses a docx file"""
-        pass
+        doc = docx.Document(file_path)
+        text = ''
+        for paragraph in doc.paragraphs:
+            text += paragraph.text + '\n'
+
+        return text
 
     def _parse_txt(self, file_path:str) -> str:
         """Parses a txt file"""
-        pass
+        with open(file_path, 'r', encoding='utf-8') as txt_file:
+            return txt_file.read()
 
     def clean_text(self, text:str) -> str:
         """Basic text cleaning"""
-        pass
+        text = ' '.join(text.split())
 
+        text = re.sub(r'[^\w\s]', '', text)
+
+        return text
